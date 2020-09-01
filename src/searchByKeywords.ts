43933bd50f80805d5a85037f6bd4b1a3224fc151
unional <homawong@gmail.com>
2020-09-01 02:44:37 -0700

    chore: add missing ignore coverage

--- src/searchByKeywords.ts
+++ src/searchByKeywords.ts
@@ -3,6 +3,7 @@ import { pickPackagesWithKeywords } from './pickPackagesWithKeywords';
 
 export function searchByKeywords(keywords: string[]): Promise<string[]>
 export function searchByKeywords(keywords: string[], fields: string[]): Promise<Record<string, any> & { name: string }[]>
+// istanbul ignore next
 export function searchByKeywords(keywords: string[], fields?: string[]) {
   return new Promise((a, r) => {
     cp.exec(`npm search --json --no-description ${keywords.join(' ')}`, (err, stdout) => {
